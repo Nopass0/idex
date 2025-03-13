032d05ed6961bb3faae93d22926592bde71ad4e2
Nopass0 <gal.bogdan2015@gmail.com>
2025-03-13 19:55:27 +0300

    minifix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const config = {
     // Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
     // for Docker builds.
+    reactStrictMode: true,
     typescript: {
         ignoreBuildErrors: true,
       },
